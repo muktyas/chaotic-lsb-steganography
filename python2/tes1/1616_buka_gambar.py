a3b05b6cb5ab99f5e96561b8c698157b378d6d9f
muktyas <muktyas@gmail.com>
2023-04-16 16:35:51 +0700

    Add files via upload

---
+++ python2/tes1/1616_buka_gambar.py
@@ -0,0 +1,88 @@
+import numpy as np
+from PIL import Image
+import time
+
+# ~ r = np.array([[200,120]])
+# ~ a = np.array([[100,50],[200,100],[150,10],[12,23],[23,34],[45,56],[67,78],[100,150]])
+
+# ~ Image.fromarray(a.astype(np.uint8)).save('cover.png')
+# ~ Image.fromarray(r.astype(np.uint8)).save('rahasia.png')
+
+def log(x0, n):
+	bar = np.zeros(n)
+	for i in range(100):
+		x0 = 3.9 * x0* (1 - x0)
+	for i in range(n):
+		x0 = 3.9 * x0* (1 - x0)
+		bar[i] = int(x0 * 1000 % 3 + 1)
+	return bar.astype(np.uint8)
+
+def buka(gb_stegano, x0, baris, kolom, banyak_bit):
+	waktu_awal = time.time()
+	s = Image.open(gb_stegano)
+	nbit_lsb = banyak_bit
+	br, kr = baris, kolom
+
+	ms = np.array(s)
+
+
+	ms_flat = ms.reshape(ms.size)
+
+	ambils = log(x0, nbit_lsb+1)
+	print ambils
+
+	jmlkum = np.cumsum(ambils)
+	print jmlkum[nbit_lsb-1], jmlkum[nbit_lsb]
+
+	selisih_bit_terakhir = jmlkum[nbit_lsb] - br * kr * 8
+	print selisih_bit_terakhir
+	# ~ print jmlkum
+
+	# ~ for i in range(nbit_rahasia):
+		# ~ if jmlkum[i] > nbit_rahasia:
+			# ~ nbit_lsb = i
+			# ~ break
+		# ~ else:
+			# ~ nbit_lsb = nbit_rahasia
+
+	# ~ print nbit_lsb
+	# ~ print
+
+	lsb_string = ''
+	for i in range(nbit_lsb):
+		# ~ ambil nbit_lsb
+		nbit = ambils[i]
+		
+		#~ print nbit
+		topeng = 2 ** nbit - 1
+		
+		#~ print 'nilai piksel:', ms_flat[i], format(ms_flat[i], '08b')
+		ambil_bit = ms_flat[i] & topeng
+		#~ print ambil_bit, format(ambil_bit, '08b')
+		
+		# jangan semua bit yang disimpan
+		sl = 8 - nbit
+		
+		ambil_bin = format(ambil_bit, '08b')
+			
+		
+		lsb_string = lsb_string + str(ambil_bin[sl:])
+		#~ print 'string_rahasia:', lsb_string
+		#~ print '-'*20, '\n'
+
+	#~ banyak_piksel = int(len(lsb_string)/8)
+	banyak_piksel = br * kr
+	mbuka = np.zeros(banyak_piksel)
+	for i in range(banyak_piksel):
+		string_pixel = lsb_string[:8]
+		mbuka[i] = int(string_pixel, 2)
+		lsb_string = lsb_string[8:]
+	print lsb_string
+	mbuka_reshape = mbuka.reshape(br, kr).astype(np.uint8)
+
+	print mbuka_reshape
+	Image.fromarray(mbuka_reshape).save('terbuka.png')
+	print
+	print 'waktu buka:', time.time() - waktu_awal,' detik.'
+
+buka('stegano.png', 0.123, 100, 100, 39213)
---
+++ python2/tes2/1609_sisip.py
@@ -0,0 +1,100 @@
+import numpy as np
+from PIL import Image
+import time
+
+# ~ r = np.array([[200,120]])
+# ~ a = np.array([[100,50],[200,100],[150,10],[12,23],[23,34],[45,56],[67,78],[100,150]])
+
+# ~ Image.fromarray(a.astype(np.uint8)).save('cover.png')
+# ~ Image.fromarray(r.astype(np.uint8)).save('rahasia.png')
+
+def log(x0, n):
+	bar = np.zeros(n)
+	for i in range(100):
+		x0 = 3.9 * x0* (1 - x0)
+	for i in range(n):
+		x0 = 3.9 * x0* (1 - x0)
+		bar[i] = x0 * 1000 % 3 + 1
+	return bar.astype(np.uint8)
+
+def sembunyikan(gb_rahasia, gb_cover, x0):
+	waktu_awal = time.time()
+	c = Image.open(gb_cover)
+	r = Image.open(gb_rahasia)
+
+	mc = np.array(c)
+	mr = np.array(r)
+
+	bc, kc = mc.shape
+	br, kr = mr.shape
+
+	mc_flat = mc.reshape(mc.size)
+	mc_flat_asli = np.copy(mc_flat)
+	mr_flat = mr.reshape(mr.size)
+
+	mr_flat_bin = np.array([ format(i,'08b') for i in mr_flat ])
+
+	#~ print mc_flat_asli
+	#~ print mr_flat_bin
+	mr_string = ''.join(mr_flat_bin)
+
+	#~ print mr_string
+
+	nbit_rahasia = 8 * br * kr
+	ambils = log(x0, nbit_rahasia)
+	#~ print ambils
+
+	jmlkum = np.cumsum(ambils)
+	#~ print jmlkum
+
+	for i in range(nbit_rahasia):
+		if jmlkum[i] > nbit_rahasia:
+			nbit_lsb = i
+			break
+		else:
+			nbit_lsb = nbit_rahasia
+
+	#~ print nbit_lsb
+	#~ print
+	#~ print 'panjang mr_string:', len(mr_string)
+
+	for i in range(nbit_lsb):
+		# ~ nolkan beberapa bit terakhir
+		nbit = ambils[i]
+		
+		#~ print nbit
+		#~ print mc_flat[i], format(mc_flat[i],'08b')
+		
+		mc_flat[i] = mc_flat[i] >> nbit << nbit
+		#~ print 'setelah dibuang ', nbit, 'menjadi', mc_flat[i], format(mc_flat[i],'08b')
+		
+		# ~ isi dengan string rahasia sebanyak n tadi
+		bit_rahasia = mr_string[:nbit]
+		int_rahasia = int(bit_rahasia, 2)
+		
+		#~ print bit_rahasia, int_rahasia
+		
+		mc_flat[i] = mc_flat[i] | int_rahasia
+		
+		mr_string = mr_string[nbit:]
+		
+		#~ print mc_flat[i]
+		#~ print mr_string
+			
+		#~ print
+	#~ print '#'*30
+	#~ print mc_flat_asli[:10], mc_flat_asli[-5:]
+	#~ print mc_flat[:10], mc_flat[-5:]
+	#~ print mr_string
+	#~ print
+	print 'x0:', x0
+	print 'baris, kolom:', br, kr
+	print 'nbit_lsb:', nbit_lsb
+
+	jadi = mc_flat.reshape(bc, kc).astype(np.uint8)
+	stegano = Image.fromarray(jadi)
+	stegano.save('stegano.png')
+	print
+	print 'waktu proses penyisipan:', time.time() - waktu_awal,' detik.'
+
+sembunyikan('cameraman.png', 'cover.png', 0.7189)
